9225dc4468087ccc6c610ccc0bd1431b856099f1
NinjaOwl - Darr H <39216431+DivoHub@users.noreply.github.com>
2022-04-02 12:45:04 -0700

    Update integration.test.js

--- server/src/test/integration.test.js
+++ server/src/test/integration.test.js
@@ -24,3 +24,30 @@ describe('/api/spotify/me', () => {
   });
 });
 
+test('Send Deezer API a playlist GET request with userID as parameter, ensure that return is Promise', () => {
+  expect(
+    typeof getDeezerUserPlaylists(4902688642) == 'object' && typeof getDeezerUserPlaylists(4902688642).then == "function"
+  ).toBe(true)
+})
+
+
+function verifyURL(){
+  const spotApi = new SpotifyWebApi({
+    clientId: 'b26491d4a12f481f9c8cd979e7151646',
+    clientSecret: '0877bb4b043b472f8ff367700b27a08b',
+    redirectUri: 'https://clefproject.herokuapp.com/login',
+   });
+   spotApi.setAccessToken('BQAJVSELjSwOeBLUCHkf2')
+   let scopes = ['streaming','user-read-private']
+   let URL = spotApi.createAuthorizeURL(scopes)
+   return URL.includes("b26491d4a12f481f9c8cd979e7151646") 
+}
+
+test('Send Spotify API an authorization URL request with client key credientials. Should return true if request returns a valid URL with the ClientID corresponding to the credentials given in the request', () => {
+  expect(
+    verifyURL()
+    ).toBe(true)
+});
+
+
+
